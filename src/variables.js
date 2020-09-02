b4a46266ab60d9b216b0ed44ab3a5964ecba5f1c
Maroun Baydoun <maroun.baydoun@gmail.com>
2020-09-02 23:19:45 +0200

    Change title

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "reaflex",
+  title: "reaflex - Easy Flexbox in React",
   description: "Easy Flexbox in React",
   heading1: "reaflex",
   heading2: "Easy Flexbox in React",
