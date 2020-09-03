c54da449aadb5ea4cb6d13e8c9a67eb4823082f0
Maroun Baydoun <maroun.baydoun@gmail.com>
2020-09-03 22:36:51 +0200

    Add links to github and npm

--- src/variables.js
+++ src/variables.js
@@ -3,6 +3,6 @@ module.exports = {
   description: "Easy Flexbox in React",
   heading1: "reaflex",
   heading2: "Easy Flexbox in React",
-  githubLink: "",
-  npmLink: "",
+  githubLink: "https://github.com/maroun-baydoun/reaflex",
+  npmLink: "https://www.npmjs.com/package/reaflex",
 };
