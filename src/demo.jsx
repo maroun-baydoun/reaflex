2d09096a6b7692589783c7ff25a8611f02b41d44
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-03-19 22:44:57 +0100

    Update demo

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 import { Flex, Item } from "reaflex";
+
+const Code = ({ code }) => {
+  const [codeVisible, setCodeVisible] = useState(false);
+  return (
+    <Fragment>
+      <pre
+        className={`syntax-highlight-container language-jsx demo-cell--code ${
+          codeVisible ? "demo-cell--code__visible" : ""
+        }`}
+      >
+        <code className="language-jsx">{code}</code>
+      </pre>
+      {!codeVisible ? (
+        <Item
+          as="button"
+          alignSelf="flex-start"
+          className="button"
+          onClick={() => setCodeVisible(true)}
+        >
+          See code
+        </Item>
+      ) : null}
+    </Fragment>
+  );
+};
 
 const Demo = () => {
   return (
@@ -13,6 +38,31 @@ const Demo = () => {
           <div className="demo-box">styled-components</div>
           <div className="demo-box">flexbox</div>
         </Flex>
+        <Code
+          code={`<Flex>
+  <div>reaflex</div>
+  <div>React</div>
+  <div>styled-components</div>
+  <div>flexbox</div>
+</Flex>`}
+        />
+      </Item>
+      <Item as={Flex} direction="column" flex={1} className="demo-cell">
+        <h4>Reverse row</h4>
+        <Flex flow="row-reverse" className="demo-cell--content">
+          <div className="demo-box">reaflex</div>
+          <div className="demo-box">React</div>
+          <div className="demo-box">styled-components</div>
+          <div className="demo-box">flexbox</div>
+        </Flex>
+        <Code
+          code={`<Flex flow="row-reverse">
+  <div>reaflex</div>
+  <div>React</div>
+  <div>styled-components</div>
+  <div>flexbox</div>
+</Flex>`}
+        />
       </Item>
       <Item as={Flex} direction="column" flex={1} className="demo-cell">
         <h4>Column</h4>
@@ -25,6 +75,34 @@ const Demo = () => {
           <div className="demo-box">styled-components</div>
           <div className="demo-box">flexbox</div>
         </Flex>
+        <Code
+          code={`<Flex direction="column">
+  <div>reaflex</div>
+  <div>React</div>
+  <div>styled-components</div>
+  <div>flexbox</div>
+</Flex>`}
+        />
+      </Item>
+      <Item as={Flex} direction="column" flex={1} className="demo-cell">
+        <h4>Reverse column</h4>
+        <Flex
+          flow="column-reverse"
+          className="demo-cell--content demo-cell--content__column"
+        >
+          <div className="demo-box">reaflex</div>
+          <div className="demo-box">React</div>
+          <div className="demo-box">styled-components</div>
+          <div className="demo-box">flexbox</div>
+        </Flex>
+        <Code
+          code={`<Flex flow="column-reverse">
+  <div>reaflex</div>
+  <div>React</div>
+  <div>styled-components</div>
+  <div>flexbox</div>
+</Flex>`}
+        />
       </Item>
     </Flex>
   );
