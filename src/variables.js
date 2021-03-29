bc80fab9e277ab7224ecfddc850b0c88895d1bdf
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-03-29 11:38:16 +0200

    Update documentation

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "reaflex - Easy Flexbox in React",
   description:
-    "Easy Flexbox in React and styled-components with the reaflex library",
+    "Create flexbox columns and rows using reaflex in your React applications. Try the demo, check the code on Github and get the package on npm.",
   heading1: "reaflex",
   heading2: "Easy Flexbox in React",
   githubLink: "https://github.com/maroun-baydoun/reaflex",
